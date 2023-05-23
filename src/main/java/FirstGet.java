4c09a6dbae345ac3073e9320bb7173e6085bda4f
ZelaOC <michal.zelga@gmail.com>
2023-05-23 09:22:15 +0200

    Test

---
+++ src/main/java/FirstGet.java
@@ -0,0 +1,17 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class FirstGet {
+    private String username = "";
+    private String password = "";
+    private String url = "";
+
+
+
+
+
+    @Test
+    public void FirstGetTest(){
+        Response response = ;
+    }
+}
